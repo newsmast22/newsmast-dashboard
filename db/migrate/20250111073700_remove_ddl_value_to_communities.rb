71174748661230b1acdfc7a3731df36107284ab7
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-08-07 23:55:30 +0630

    delete safety_assured from migration files

--- db/migrate/20250111073700_remove_ddl_value_to_communities.rb
+++ db/migrate/20250111073700_remove_ddl_value_to_communities.rb
@@ -1,8 +1,6 @@
 class RemoveDdlValueToCommunities < ActiveRecord::Migration[7.1]
   def change
-    safety_assured do
-      remove_column :patchwork_communities, :ddl_value
-      add_column :patchwork_communities, :did_value, :string, default: nil, null: true
-    end
+    remove_column :patchwork_communities, :ddl_value
+    add_column :patchwork_communities, :did_value, :string, default: nil, null: true
   end
 end
